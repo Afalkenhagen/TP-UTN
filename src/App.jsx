7bd8b2e14416a6d580fadbf029ff9b1cbcc51adb
Afalkenhagen <ahau_buluk@protonmail.ch>
2023-11-20 19:26:17 -0300

    style: new card for TasksManager

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-     <Routes>
-       <Route path="/" element={<Bienvenida />} />
-       <Route path="/app" element={<TasksManager />} />
-     </Routes>
-   </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Bienvenida />} />
+      <Route path="/app" element={
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+          <div className="w-full max-w-md p-8 m-4 bg-white rounded shadow-md">
+            <TasksManager />
+          </div>
+        </div>
+      } />
+    </Routes>
+  </Router>
   );
-}
+ }
 
 export default App
